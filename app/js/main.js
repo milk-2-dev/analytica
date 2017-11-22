f87db185e8d1a83cfd63efbe479d0b08b58fe7d0
Roman <papachiv.roma@gmail.com>
2017-11-23 00:54:09 +0200

    90%

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,30 @@
 function animateMobile(){
     var animateAddClass = setInterval(function() {
         $('.header-number i').addClass('tada');
-    }, 2000);
+    }, 3000);
 
     var animateRemoveClass = setInterval(function() {
         $('.header-number i').removeClass('tada');
-    }, 4000);
+    }, 6000);
 
 }
 
 animateMobile();
+
+function customCollapse(){
+    $('.setting-item__desc a').on('click', function(e){
+        if($(this).parent().is('.open')){
+            $(this).parent().css('max-height', '80px');
+            $(this).parent().removeClass('open');
+            $(this).css('top', '55px');
+        }
+        else{
+            var blockHeight = $(this).parent().find('p').outerHeight(true);
+            $(this).parent().addClass('open');
+            $(this).parent().css('max-height', blockHeight);
+            $(this).css('top', blockHeight - 20);
+        }
+    })
+}
+
+customCollapse()
